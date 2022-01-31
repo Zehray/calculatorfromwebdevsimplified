da999dc33191ce8bbf2fd0a976ed01498e3d6fe2
Zehray <zehrayatki@gmail.com>
2022-01-31 19:58:26 +0300

    current Operand executed

--- notes.js
+++ notes.js
@@ -29,4 +29,64 @@
    classes since already used by our CSS.
    We added data-previous-operand and data-current-operand
 
+   
+   
+   
+   *We need to know the previous operand the user entered the current operand 
+    that they're working on and the operation that they've if any.
+
+    inside the clear function all we need to do is remove all of these values.
+
+    the first thing we can do to just say this.currentOperand = '' which is just going to be what is defined by this number right here. We just want to default this to an empty string if they cleared.
+
+    And Lastly we want to change the operation to be undefined since they don't have any operation selected if we clear things.
+
+    And as soon as we create our calculator we want to call this function ( this.clear() ) and this is because we want to clear all of our inputs and set them all to the default values as soon as we create a new calculator.
+
+    So now that we know all the different things that our calculator can do defined bu our functions and all the different variables that it can hold.
+
     */
+/*1- The first thing that we need to think about is 
+   how are we going to store all the information 
+   for what's number currently typed here what number is currently typed here
+   what operation is it and I think easiest way to do that is to use a class.
+   so we're going to create a calculator class .
+
+   Let's go in here and create a class which is called a calculator and inside of this class
+   we're going to put a constructor which is going to take all the inputs 
+   for it as well as all the functions for our calculator.
+   */
+
+   /*
+   so, our constructor is essentially just going to take our previous operand text element and
+   our current text element and this is because 
+   we need to know where to place our display text for our calculator.
+
+   
+
+2-
+next we want to think about all the operations our calculator class can come perform.
+
+
+*/
+
+    /*
+3-let's we think about the different properties our calculator needs to store.
+* 3-1- We need to know the previous operand the user entered the current operand 
+that they're working on and the operation that they've if any.
+
+* 3-2-  inside the clear function all we need to do is 
+remove all of these values 
+
+the first thing we can do to just say this.currentOperand = '' 
+which is just going to be what is defined by this number right here.
+ We just want to default this to an empty string if they cleared.
+ And we want to do same thin for previousOperand again defaulted to an empty string.
+
+*/
+
+// let's work on hooking up all of our different variables and making them operate on our calculator
+
+//First thing we need to do is create a calculator,we're trying to set this equal to a new calculator
+//which is how you define classes you say new followed by the class name and we pass everythin from our constructor into it
+//in our case we need to pass it the previousOperandTextElement and currentOperantTextElement
--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-/*1- The first thing that we need to think about is 
-   how are we going to store all the information 
-   for what's number currently typed here what number is currently typed here
-   what operation is it and I think easiest way to do that is to use a class.
-   so we're going to create a calculator class .
 
-   Let's go in here and create a class which is called a calculator and inside of this class
-   we're going to put a constructor which is going to take all the inputs 
-   for it as well as all the functions for our calculator.
-   */
-
-   /*
-   so, our constructor is essentially just going to take our previous operand text element and
-   our current text element and this is because 
-   we need to know where to place our display text for our calculator.
-
-2-
-next we want to think about all the operations our calculator class can come perform.
-
-*/
 
 //1
    class Calculator {
        constructor(previousOperandTextElement,currentOperandTextElement){
+        this.clear()
            //inside of here we can set some variables for this class 
            //this essentially gives us a way to set these text elements inside of our calculator class
            this.previousOperandTextElement = previousOperandTextElement 
@@ -29,20 +11,23 @@ next we want to think about all the operations our calculator class can come per
        }
 //2-We have the clearing function, the delete function, the adding number also we have the equals function and the adding of an operation.
        clear() { //this is just going to clear out our different variables.
-           this.currenOperand = ''
+           this.currentOperand = ''
            this.previousOperand = ''
            this.operation = undefined //since they don't have any operation selected if we clear things
-           this.clear()
-       }
+            //And as soon as we create our calculator we want to call this function ( this.clear() )
+                        // and this is because we want to clear all of our inputs and set them all to the default values as soon as we create a new calculator.
+       };
 
        delete() { //Next we're going to have our delete function for removing a single number.
 
-       }
-
-       appendNumber(number) { //We're going to create a function called appendNumber which is essentially 
+       };
+       
+        appendNumber(number) { //We're going to create a function called appendNumber which is essentially 
                               //what's going to happen every single time a user clicks on a number to add to the screen. 
 
-       }
+            this.currentOperand =  this.currentOperand.toString() + number.toString()
+
+       };
 
        chooseOperation(operation)  /*Next thing we need to do is to chooseOperation function.
                                     This is what's going to happen anytime a user clicks one of 
@@ -62,6 +47,7 @@ next we want to think about all the operations our calculator class can come per
 
        updateDisplay() //we need to have a function that allows us to update our display and this is going to update the values inside of our output.
        {
+           this.currentOperandTextElement.innerText = this.currentOperand
 
        }
    }
@@ -74,13 +60,12 @@ next we want to think about all the operations our calculator class can come per
    const previousOperandTextElement = document.querySelector('[data-previous-operand]')
    const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
-/*
-3-let's we think about the different properties our calculator needs to store.
-* 3-1- We need to know the previous operand the user entered the current operand 
-that they're working on and the operation that they've if any.
 
-* 3-2-  inside the clear function all we need to do is 
-remove all of these values the first thing we can do to just this.currentOperand = ''
-we just want to default this to an empty string if they cleared, 
-we want to do the same thing for thiss.previousOperand = ''
-*/
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
